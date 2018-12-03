766416dc9b43b6453511f604bcc00b0be7d08e46
Gregory Wiedeman <gw234478@lib-espy-ws-d101.its.albany.edu>
2018-12-03 16:50:16 -0500

    changed back to serve under root path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,8 +25,6 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-  
-  config.assets.prefix = '/repository/pipeline_assets'
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
