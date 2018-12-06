faa979073433f1ddf343d7da34bc786c29c8af1d
Gregory Wiedeman <gw234478@lib-espy-ws-p101.its.albany.edu>
2018-12-06 13:00:06 -0500

    Revert "changed back to serve under root path"
    This reverts commit 766416dc9b43b6453511f604bcc00b0be7d08e46.

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,42 @@
 Rails.application.routes.draw do
+  scope 'repository' do
   
-  mount Riiif::Engine => 'images', as: :riiif if Hyrax.config.iiif_image_server?
-  mount Blacklight::Engine => '/'
-  
-    concern :searchable, Blacklight::Routes::Searchable.new
+      mount Riiif::Engine => 'images', as: :riiif if Hyrax.config.iiif_image_server?
+      mount Blacklight::Engine => '/'
+      
+        concern :searchable, Blacklight::Routes::Searchable.new
 
-  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
-    concerns :searchable
-  end
+      resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+        concerns :searchable
+      end
 
-  devise_for :users
-  mount Hydra::RoleManagement::Engine => '/'
+      devise_for :users
+      mount Hydra::RoleManagement::Engine => '/'
 
-  mount Qa::Engine => '/authorities'
-  mount Hyrax::Engine, at: '/'
-  resources :welcome, only: 'index'
-  root 'hyrax/homepage#index'
-  curation_concerns_basic_routes
-  concern :exportable, Blacklight::Routes::Exportable.new
+      mount Qa::Engine => '/authorities'
+      mount Hyrax::Engine, at: '/'
+      resources :welcome, only: 'index'
+      root 'hyrax/homepage#index'
+      curation_concerns_basic_routes
+      concern :exportable, Blacklight::Routes::Exportable.new
 
-  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
-    concerns :exportable
-  end
+      resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+        concerns :exportable
+      end
 
-  resources :bookmarks do
-    concerns :exportable
+      resources :bookmarks do
+        concerns :exportable
 
-    collection do
-      delete 'clear'
-    end
-  end
+        collection do
+          delete 'clear'
+        end
+      end
 
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+      require 'sidekiq/web'
+      mount Sidekiq::Web => '/sidekiq'
 
-  mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
+      mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
+   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
