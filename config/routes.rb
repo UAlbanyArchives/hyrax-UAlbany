4f96a5fd3083cb01e8af6d342a700db1640aace9
Gregory Wiedeman <gw234478@lib-espy-ws-d101.its.albany.edu>
2018-12-18 11:07:33 -0500

    restricted sidekiq console to admins

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,9 @@ Rails.application.routes.draw do
   end
 
   require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
 
