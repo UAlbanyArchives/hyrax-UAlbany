42b8e95e81de805cf0d08cfff8c45d2828697f30
root <root@lib-espy-ws-p101.its.albany.edu>
2018-12-24 12:03:57 -0500

    fixed breadcrumb links

--- app/assets/javascripts/displayArclightData.js
+++ app/assets/javascripts/displayArclightData.js
@@ -12,8 +12,11 @@ $( document ).on('turbolinks:load', function() {
 			  		$query = '.arclightReify:eq(' + String(i) + ')';
 			  		$parentLink = $('.arclightBreadcrumbs').children($query);
 		  			$urlRoot = $parentLink.children("a").attr("href").split("aspace_")[0];
-		  			/*$parentLink.children("a").attr("href", $urlRoot + data['response']['document']['parent_ssm'][i])*/
-		  			$parentLink.children("a").attr("href", $urlRoot)
+		  			if ( i == 0 ) {
+						$parentLink.children("a").attr("href", $urlRoot)
+					} else {
+						$parentLink.children("a").attr("href", $urlRoot + data['response']['document']['parent_ssm'][i])
+		  			}
 		  			$parentLink.children("a").text(data['response']['document']['parent_unittitles_ssm'][i])
 			  	}
 			  }
