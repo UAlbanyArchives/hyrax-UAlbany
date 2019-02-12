71fe9b945939ae5a092fb5b99d18b66ad9619f01
Gregory Wiedeman <gw234478@lib-espy-ws-d101.its.albany.edu>
2019-02-12 16:20:17 -0500

    swiched search subnac to match arclight

---
+++ app/assets/javascripts/searchHandler.js
@@ -0,0 +1,70 @@
+window.addEventListener('load', function() {
+
+    // URLs for Search form get request
+    var allSearchURL = "https://archives.albany.edu/search";
+    var arclightSearchURL = "https://archives.albany.edu/collections/catalog";
+    var hyraxSearchURL = "https://archives.albany.edu/catalog";
+
+    //query selector for search form
+    var searchForm = document.getElementsByClassName('search_q');
+    var searchPlaceholder = document.getElementById('params-q');
+    var currentSelection = document.getElementById('currentSelection');
+
+
+    //button query selectors
+    var allSearchBtn = document.getElementById('allSearch')
+    var arclightBtn = document.getElementById('arclightSearch');
+    var hyraxBtn = document.getElementById('hyraxSearch');
+    
+    var caret = document.createElement("span");
+    caret.setAttribute("class", "caret");
+
+    //
+    //
+    // Event Listeners - Search options change
+    //
+    //
+
+    //Search all
+    allSearchBtn.addEventListener('click', function() {
+        searchForm[0].action = allSearchURL;
+        searchPlaceholder.placeholder = "Search everything...";
+        currentSelection.textContent = "Everything ";
+        currentSelection.appendChild(caret)
+    });
+
+    //Search Arclight
+    arclightBtn.addEventListener('click', function() {
+        searchForm[0].action = arclightSearchURL;
+        searchPlaceholder.placeholder = "Search all collection records...";
+        currentSelection.textContent = "Collections ";
+        currentSelection.appendChild(caret)
+    });
+
+    //Search Hyrax
+    hyraxBtn.addEventListener('click', function() {
+        searchForm[0].action = hyraxSearchURL;
+        searchPlaceholder.placeholder = "Search only online digital content...";
+        currentSelection.textContent = "Digital Selections ";
+        currentSelection.appendChild(caret)
+    });
+
+
+}, false);
+
+
+window.addEventListener('load', function() {
+    $('#toggleSearch').click( function() {
+        $("#searchSub").toggleClass("fixedSubnav");
+        $("#content-wrapper").toggleClass("subnavSpacer");
+    } );
+
+    $(window).scroll(function () {
+        var top_offset = $(window).scrollTop();
+        if (top_offset == 0) {
+            $('#searchSub').removeClass('fixedSubnav');
+            $('#content-wrapper').removeClass('subnavSpacer');
+         }
+    } );
+});
+
