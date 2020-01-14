1a4e149332c912684ec48df339f7254719c47951
gwiedeman <gregory.wiedeman1@gmail.com>
2020-01-14 11:51:02 -0500

    added mailign setting to maybe avoild 500 when resetting a password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "hyrax_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.default_url_options = { :host => 'archives.albany.edu' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
