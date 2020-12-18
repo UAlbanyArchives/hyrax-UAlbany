78f6cf9e28975286551760c4ef22d62c3781c985
Katherine Mules <kmules@albany.edu>
2020-12-18 16:04:02 -0500

    enforce ssl for iiif

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -24,3 +24,6 @@ Riiif.not_found_image = Rails.root.join('app', 'assets', 'images', 'us_404.svg')
 Riiif.unauthorized_image = Rails.root.join('app', 'assets', 'images', 'us_404.svg')
 
 Riiif::Engine.config.cache_duration_in_days = 365
+
+# Maybe fix issue with Univesal Viewer not loading https://github.com/samvera/hyrax/issues/4572
+Riiif::Engine.routes.default_url_options = { protocol: 'https' }
