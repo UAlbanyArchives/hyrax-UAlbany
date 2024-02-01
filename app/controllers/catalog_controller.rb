2ae832616c3c436386cfdc1f71abff8a26cf5e18
Greg Wiedeman <gregory.wiedeman1@gmail.com>
2024-02-01 12:24:17 -0500

    adds thumbnail path and modified date to the API

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -97,6 +97,10 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name("collecting_area", :stored_searchable), label: "Collecting Area", link_to_facet: solr_name("collecting_area", :facetable)
     config.add_index_field solr_name("collection_number", :stored_searchable), label: "Collection ID", link_to_facet: solr_name("collection_number", :facetable)
     config.add_index_field solr_name("record_parent", :stored_searchable), label: "Parent Record(s)", link_to_facet: solr_name("record_parent", :facetable)
+
+    config.add_index_field "thumbnail_path_ss"
+    config.add_index_field "system_modified_dtsi"
+
     #config.add_index_field solr_name("accession", :stored_searchable), label: "Accession", link_to_facet: solr_name("accession", :facetable)
     config.add_index_field solr_name("description", :stored_searchable), itemprop: 'description', helper_method: :iconify_auto_link
     config.add_index_field solr_name("keyword", :stored_searchable), itemprop: 'keywords', link_to_facet: solr_name("keyword", :facetable)
